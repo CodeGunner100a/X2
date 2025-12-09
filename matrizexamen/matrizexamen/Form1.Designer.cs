e308d064bec6aae2da76808d3333d96034286727
CodeGunner100a <codegunner178@gmail.com>
2025-12-09 08:37:00 -0400

    Add files via upload

---
+++ matrizexamen/matrizexamen/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace matrizexamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mayorFrecuanciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuX1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.descargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Location = new System.Drawing.Point(86, 403);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(363, 13);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(426, 155);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(298, 225);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(86, 155);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(298, 225);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(474, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(335, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuX1ToolStripMenuItem,
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(858, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mayorFrecuanciaToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.menuToolStripMenuItem.Text = "Menu Examen";
+            // 
+            // mayorFrecuanciaToolStripMenuItem
+            // 
+            this.mayorFrecuanciaToolStripMenuItem.Name = "mayorFrecuanciaToolStripMenuItem";
+            this.mayorFrecuanciaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mayorFrecuanciaToolStripMenuItem.Text = "Ejercicio 1";
+            this.mayorFrecuanciaToolStripMenuItem.Click += new System.EventHandler(this.mayorFrecuanciaToolStripMenuItem_Click);
+            // 
+            // menuX1ToolStripMenuItem
+            // 
+            this.menuX1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarToolStripMenuItem,
+            this.descargarToolStripMenuItem});
+            this.menuX1ToolStripMenuItem.Name = "menuX1ToolStripMenuItem";
+            this.menuX1ToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.menuX1ToolStripMenuItem.Text = "Menu x1";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            this.cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            this.cargarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cargarToolStripMenuItem.Text = "cargar";
+            this.cargarToolStripMenuItem.Click += new System.EventHandler(this.cargarToolStripMenuItem_Click);
+            // 
+            // descargarToolStripMenuItem
+            // 
+            this.descargarToolStripMenuItem.Name = "descargarToolStripMenuItem";
+            this.descargarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.descargarToolStripMenuItem.Text = "descargar";
+            this.descargarToolStripMenuItem.Click += new System.EventHandler(this.descargarToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 485);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mayorFrecuanciaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuX1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem descargarToolStripMenuItem;
+    }
+}
+
---
+++ matrizexamen/matrizexamen/Matriz.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrizexamen
+{
+    class Matriz
+    {
+    
+        const int MAXF = 50;
+        const int MAXC = 50;
+
+        private int[,] x;
+
+        private int f, c;
+
+   
+
+        public Matriz()
+        {
+
+            x = new int[MAXF, MAXC];
+            f = 0;
+            c = 0;
+
+        }
+
+
+
+        public void CargarRandom(int nf, int nc, int a, int b)
+
+        {
+            int f1, c1;
+            Random r = new Random();
+            f = nf;
+            c = nc;
+
+
+            for (f1 = 1; f1 <= f; f1++)
+
+            {
+                for (c1 = 1; c1 <= c; c1++)
+
+                {
+                    x[f1, c1] = r.Next(a, b + 1);
+
+                }
+
+            }
+
+        }
+
+
+        public string Descargar()
+
+        {
+            string s = "";
+            int f1, c1;
+            for (f1 = 1; f1 <= f; f1++)
+
+            {
+                for (c1 = 1; c1 <= c; c1++)
+
+                {
+                    s = s + x[f1, c1] + "\x9";
+
+
+                }
+                s = s + "\xd" + "\xa";
+
+            }
+
+            return s;
+
+        }
+
+
+        //examen
+
+        public int ejercicio1matricess()
+        {
+            int ned = 0;
+
+           
+            for (int f1 = 1; f1 <= f; f1++)
+            {
+                for (int c1 = c - f + f1; c1 <= c; c1++)
+                {
+                    int actual = x[f1, c1];
+                    bool esDiferente = true;
+                    bool salir = false;
+
+                  
+                    for (int f2 = 1; f2 <= f && !salir; f2++)
+                    {
+                        for (int c2 = c - f + f2; c2 <= c && !salir; c2++)
+                        {
+                           
+                            if (f2 == f1 && c2 == c1)
+                            {
+                                salir = true;
+                            }
+                            else if (x[f2, c2] == actual)
+                            {
+                                
+                                esDiferente = false;
+                                salir = true;
+                            }
+                        }
+                    }
+
+                    if (esDiferente)
+                        ned++;
+                }
+            }
+
+            return ned;
+        }
+
+
+        //-----------------
+
+
+        public void MayorFrecuenciaTSupDer(ref int ele, ref int frec)
+        {
+            ele = 0;
+            frec = 0;
+
+            for (int f1 = 1; f1 <= f; f1++)
+            {
+                for (int c1 = f1; c1 <= c; c1++)  
+                {
+                    int cont = 0;
+                    int val = x[f1, c1];
+
+
+                    for (int f2 = 1; f2 <= f; f2++)
+                    {
+                        for (int c2 = f2; c2 <= c; c2++)
+                        {
+                            if (x[f2, c2] == val)
+                                cont++;
+                        }
+                    }
+
+                    if (cont > frec)
+                    {
+                        frec = cont;
+                        ele = val;
+                    }
+                }
+            }
+        }
+
+    }
+}
